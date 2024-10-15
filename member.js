1806d431b5c7c2fbc5a884143f9751dd7c0bab0b
DEEP BHATTACHARJEE <deepasn2001@gmail.com>
2024-10-15 06:18:35 -0700

    updated

--- member.js
+++ member.js
@@ -18,3 +18,83 @@
   window.addEventListener('scroll', revealOnScroll);
   window.addEventListener('load', revealOnScroll); // Trigger on page load
 
+
+
+
+  // /swiper
+  // Swiper Initialization
+  const swiper1 = new Swiper('.swiper-container-1', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    effect: 'slide',
+    slidesPerView: 1,
+    spaceBetween: 20,
+    on: {
+        slideChangeTransitionStart: function () {
+            const slides = document.querySelectorAll('.swiper-container-1 .swiper-slide');
+            slides.forEach((slide) => {
+                slide.style.opacity = '0';
+            });
+            const activeSlide = slides[this.activeIndex];
+            activeSlide.style.opacity = '1';
+        },
+        slideChangeTransitionEnd: function () {
+            const slides = document.querySelectorAll('.swiper-container-1 .swiper-slide');
+            slides.forEach((slide) => {
+                slide.style.transform = '';
+            });
+            const nextIndex = (this.activeIndex + 1) % slides.length;
+            const nextSlide = slides[nextIndex];
+            nextSlide.style.transform = 'translateX(0)';
+        },
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
+
+const swiper2 = new Swiper('.swiper-container-2', {
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    effect: 'slide',
+    slidesPerView: 1,
+    spaceBetween: 20,
+    on: {
+        slideChangeTransitionStart: function () {
+            const slides = document.querySelectorAll('.swiper-container-2 .swiper-slide');
+            slides.forEach((slide) => {
+                slide.style.opacity = '0';
+            });
+            const activeSlide = slides[this.activeIndex];
+            activeSlide.style.opacity = '1';
+        },
+        slideChangeTransitionEnd: function () {
+            const slides = document.querySelectorAll('.swiper-container-2 .swiper-slide');
+            slides.forEach((slide) => {
+                slide.style.transform = '';
+            });
+            const nextIndex = (this.activeIndex + 1) % slides.length;
+            const nextSlide = slides[nextIndex];
+            nextSlide.style.transform = 'translateX(0)';
+        },
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+});
